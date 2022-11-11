29dbd125d939bb81768d0332553e110a97f53bd3
AntonioJesusMoreno <Latroes123@gmail.com>
2022-11-11 14:38:00 +0100

    Finalizacion

--- components/Formulario1.jsx
+++ components/Formulario1.jsx
@@ -18,10 +18,11 @@ const Formulario1 = () =>{
     const [sexo, setSexo]=useState(true);
     const [cambio, setCambio]=useState(true);
 
-    const nombre = useRef(null);
-    const apellidos = useRef(null);
-    const edad = useRef(null);
-    const correoElectronico = useRef(null);
+    const [nombre,setNombre] = useState();
+    const [apellidos, setApellidos] =useState();
+    const [edad,setEdad] = useState();
+    const [email, setEmail] = useState();
+    const [feedback, setFeedback] = useState([]);
     
     let elements =[];
 
@@ -30,10 +31,10 @@ const Formulario1 = () =>{
     const styles = StyleSheet.create({
         inputBox:{
             borderColor:'black',
-             width:150, height:45, 
-             borderWidth: 2,
-             borderRadius:10,
-             marginLeft:20,
+            width:150, height:45, 
+            borderWidth: 2,
+            borderRadius:10,
+            marginLeft:20,
         },
         box:{
             flexDirection:'row',
@@ -46,48 +47,64 @@ const Formulario1 = () =>{
             fontWeight:'900',
             width: 120,
             textAlign:'left'
+        },
+        feedBox:{
+            justifyContent:'center',
+            alignItems:'center',
+            marginTop:30,
+        },
+        feed:{
+            marginLeft:50,
+            marginRight:50,
+        },
+        feedImagen:{
+            width:50, 
+            height:50,
+        },
+        red:{
+           color:"red" 
         }
     
     });
-    if(correoElectronico.value == null ){
+
+    if(email == null ){
 
     }
-    else if(!regex.test(correoElectronico.value)){
-        elements.push(<Text>Correo Incorrecto</Text>)
+    else if(!regex.test(email)){
+        elements.push(<Text  style={styles.feed, styles.red} >Correo Incorrecto</Text>)
 
     }
-    else if(edad<1){
-        elements.push(<Text>Edad Incorrecta</Text>)
+    else if(isNaN(+edad)){
+        elements.push(<Text  style={styles.feed, styles.red}>Edad Incorrecta</Text>)
     }else{
-    elements.push(<Text>Mi nombre es {nombre.value},{apellidos.value} con edad {edad.value} mi correo es {correoElectronico.value} y soy {sexo? "Hombre": "Mujer"}</Text>)
-    elements.push(<Image source ={img} style={{width:50, height:50}}/>)
+    elements.push(<Text  style={styles.feed}>Mi nombre es {nombre} {apellidos} con edad {edad}. Mi correo es {email} y soy {sexo? "Hombre": "Mujer"}</Text>)
+    elements.push(<Image style={styles.feedImagen} source ={img} style={{}}/>)
     }
-console.log(correoElectronico)
 
 return(
 <>
 <View style={[styles.box,{marginTop:50}]}>
     <Text style={styles.text} >Nombre</Text>
     <View style={styles.inputBox}>
-        <TextInput ref={nombre} style={{ flex:1}}/>
+        <TextInput onChangeText={(value)=>setNombre(value)} style={{ flex:1}}/>
     </View>
 </View>
 <View style={styles.box}>
     <Text style={styles.text}>Apellidos</Text>
     <View style={styles.inputBox}>
-        <TextInput ref={apellidos} style={{ flex:1}}/>
+        <TextInput onChangeText={(value)=>setApellidos(value)} style={{ flex:1}}/>
     </View>
 </View>
 <View style={styles.box}>
     <Text style={styles.text}>Edad</Text>
     <View style={styles.inputBox}>
-        <TextInput ref={edad} keyboardType='numeric' style={{ flex:1}}/>
+        <TextInput  onChangeText={(value)=>setEdad(value)} keyboardType='numeric' style={{ flex:1}}/>
     </View>
 </View>
 <View style={styles.box}>
     <Text style={styles.text}>Correo Electronico</Text>
     <View style={styles.inputBox}>
-        <TextInput ref={correoElectronico} style={{ flex:1}}/>
+        <TextInput  onChangeText={(value)=>setEmail(value)} keyboardType='email-address' style={{ flex:1}}/>
     </View>
 </View>
 <View style={styles.box}>
@@ -100,11 +117,12 @@ return(
 <View  style={{ alignItems:'center'}}>
 <View style={{width:100,borderRadius:15, borderWidth:1, flexDirection: 'row',
           justifyContent: 'center', backgroundColor:"blue"}}>
-          <Button  color="blue" title="  Enviar  " onPress={() => {setCambio(current=> !current)}}></Button>
+          <Button  color="blue" title="  Enviar  " onPress={() => {setFeedback(elements)}}></Button>
 </View>
 </View>
-{elements}
-
+<View style={styles.feedBox}>
+{feedback}
+</View>
 </>
 );
 
